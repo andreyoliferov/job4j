13394c72744b4ae04c4baed09fed9dd91f7d8ca6
andreyoliferov <a.oliferov@gmail.com>
2018-04-28 22:16:21 +0600

    1.3. JavDoc. Code convetion.[#51343]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,7 +1,17 @@
 package ru.job4j.calculate;
 
+/**
+* Class Calculate решение задания части 001 урок 1
+* @autor Андрей Олиферов
+* @since 27.04.2018
+* @versin 1.0
+*/
 public class Calculate {
 	
+	/**
+	* Конструктор, базовый метод
+	* @param args args
+	*/
 	public static void main (String [] args) {
 		System.out.println("Hellow World!");
 	}
---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Andrey Oliferov (a.oliferov@gmail.com)
+* @version $Id$
+* @since 0.1
+*/
+package ru.job4j;
