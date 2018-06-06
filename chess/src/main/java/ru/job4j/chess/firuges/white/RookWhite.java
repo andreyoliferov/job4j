46b7ead51d5c6ca0d8669c0e6d45296a16527b9e
andreyoliferov <a.oliferov@gmail.com>
2018-06-06 23:00:07 +0600

    2018.06.06 ладья

--- chess/src/main/java/ru/job4j/chess/firuges/white/RookWhite.java
+++ chess/src/main/java/ru/job4j/chess/firuges/white/RookWhite.java
@@ -1,5 +1,6 @@
 package ru.job4j.chess.firuges.white;
 
+import ru.job4j.chess.exceptions.ImpossibleMoveException;
 import ru.job4j.chess.firuges.Cell;
 import ru.job4j.chess.firuges.Figure;
 
@@ -20,8 +21,27 @@ public class RookWhite implements Figure {
     }
 
     @Override
-    public Cell[] way(Cell source, Cell dest) {
-        return new Cell[] { dest };
+    public Cell[] way(Cell source, Cell dest) throws ImpossibleMoveException {
+        int a = dest.x - source.x;
+        int b = dest.y - source.y;
+        Cell[] array = new Cell[Math.abs(a) > Math.abs(b) ? Math.abs(a) : Math.abs(b)];
+        if (!(a == 0 && b != 0 || a != 0 && b == 0)) {
+            throw new ImpossibleMoveException();
+        }
+        int step = Integer.compare(b == 0 ? a : b, 0);
+        int delta = step;
+        if (b == 0) {
+            for (int i = 0; i < array.length; i++) {
+                array[i] = Cell.find(source.x + delta, source.y);
+                delta += step;
+            }
+        } else {
+            for (int i = 0; i < array.length; i++) {
+                array[i] = Cell.find(source.x, source.y + delta);
+                delta += step;
+            }
+        }
+        return array;
     }
 
     @Override
