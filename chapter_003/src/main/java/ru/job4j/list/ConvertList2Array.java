f7e006a087a3c3eaa28bbf2158a8e53f3716f9f9
andreyoliferov <a.oliferov@gmail.com>
2018-06-30 00:01:19 +0600

    1. Конвертация листа массивов в один лист Integer[#59238]

--- chapter_003/src/main/java/ru/job4j/list/ConvertList2Array.java
+++ chapter_003/src/main/java/ru/job4j/list/ConvertList2Array.java
@@ -1,5 +1,7 @@
 package ru.job4j.list;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -22,5 +24,15 @@ public class ConvertList2Array {
             }
         }
         return array;
+    }
+
+    public List<Integer> convert (List<int[]> list) {
+        List<Integer> result = new ArrayList<>();
+        for (int[] array : list) {
+            for (int in : array) {
+                result.add(in);
+            }
+        }
+        return result;
     }
 }
--- chapter_003/src/test/java/ru/job4j/list/ConvertList2ArrayTest.java
+++ chapter_003/src/test/java/ru/job4j/list/ConvertList2ArrayTest.java
@@ -2,7 +2,9 @@ package ru.job4j.list;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
@@ -13,10 +15,11 @@ import static org.junit.Assert.assertThat;
  */
 public class ConvertList2ArrayTest {
 
+    private ConvertList2Array convertList = new ConvertList2Array();
+
     @Test
     public void when7ElementsThen9() {
-        ConvertList2Array list = new ConvertList2Array();
-        int[][] result = list.toArray(
+        int[][] result = convertList.toArray(
                 Arrays.asList(1, 2, 3, 4, 5, 6, 7),
                 3
         );
@@ -26,5 +29,15 @@ public class ConvertList2ArrayTest {
                 {7, 0, 0}
         };
         assertThat(result, is(expect));
+    }
+
+    @Test
+    public void when3ArrayConvertArrayList() {
+        List<int[]> list = new ArrayList<>();
+        list.add(new int[]{1, 2});
+        list.add(new int[]{3, 4, 5, 6});
+        list.add(new int[]{7, 8, 9});
+        List<Integer> result = convertList.convert(list);
+        assertThat(result, is(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9)));
     }
 }
