b1a8228b76b707410c4cfd0d611378e0eec30df4
andreyoliferov <a.oliferov@gmail.com>
2018-08-06 21:47:29 +0600

    1. Пин-понг. [#1016]

---
+++ chapter_012_Multithreading/src/main/java/threads/RectangleMove.java
@@ -0,0 +1,37 @@
+package threads;
+
+import javafx.scene.shape.Rectangle;
+
+/**
+ * @autor Андрей
+ * @since 06.08.2018
+ */
+public class RectangleMove implements Runnable {
+
+    private final Rectangle rect;
+    private int x = 1;
+    private int y = 1;
+
+    public RectangleMove(Rectangle rect) {
+        this.rect = rect;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            if (rect.getX() == 0 || rect.getX() == 300) {
+                x *= -1;
+            }
+            if (rect.getY() == 0 || rect.getY() == 300) {
+                y *= -1;
+            }
+            this.rect.setX(this.rect.getX() + x);
+            this.rect.setY(this.rect.getY() + y);
+            try {
+                Thread.sleep(20);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
