ae0e9a1ea751a4c419bd3a662b80099cf5e30c16
Andrey Oliferov <a.oliferov@gmail.com>
2018-11-17 13:57:27 +0600

    1. Создать web проект [#2514]

---
+++ chapter_014_Serlvet_JSP/src/main/java/example/servlets/ExampleServlet.java
@@ -0,0 +1,28 @@
+package example.servlets;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+/**
+ * @autor aoliferov
+ * @since 17.11.2018
+ */
+public class ExampleServlet extends HttpServlet {
+    private static final Logger LOG = LoggerFactory.getLogger(ExampleServlet.class);
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter writer = new PrintWriter(resp.getOutputStream());
+        writer.append("hello");
+        writer.flush();
+    }
+}
