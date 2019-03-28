1479bc86bdd5442c60af8e2d06c217e61ee464ac
Andrey Oliferov <a.oliferov@gmail.com>
2019-03-28 11:38:13 +0600

    28.03.2019 paving

--- chapter_001/src/main/java/ru/job4j/Task.java
+++ chapter_001/src/main/java/ru/job4j/Task.java
@@ -6,5 +6,26 @@ package ru.job4j;
  */
 public class Task {
 
+    private int paving(int a, int b, int start) {
+        if (a > b) {
+            a = a - b;
+        } else {
+            b = b - a;
+        }
+        start++;
+        if (a == 0 || b == 0) {
+            return start;
+        }
+        return paving(a, b, start);
+    }
+
+    public int paving(int a, int b) {
+        return paving(a, b, 0);
+    }
+
+    public static void main(String[] args) {
+        int result = new Task().paving(4, 3);
+        System.out.println();
+    }
 
 }
